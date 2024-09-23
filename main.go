07f1d21ad05b93a7a692a94d8593fd178ae73f3c
Pedro Previatti <pvlpreviatti@gmail.com>
2024-09-23 14:09:06 -0300

    Complex Proto Message

--- main.go
+++ main.go
@@ -15,6 +15,20 @@ func doSimple() *pb.Simple {
 	}
 }
 
+func doComplex() *pb.Complex {
+	return &pb.Complex{
+		OneDummy: &pb.Dummy{Id: 100, Name: "Dummy 100"},
+		MultipleDummies: []*pb.Dummy{
+			{Id: 101, Name: "Dummy 101"},
+			{Id: 102, Name: "Dummy 102"},
+		},
+	}
+}
+
 func main() {
-	fmt.Print(doSimple())
+	fmt.Println("Simple Proto Message")
+	fmt.Println(doSimple())
+
+	fmt.Println("Complex Proto Message")
+	fmt.Println(doComplex())
 }
